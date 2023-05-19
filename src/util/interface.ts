7dd9a4f506c4eaa9e3e3b2ed4c69bf3962c2235f
seongjun.oh <tiger8708@gmail.com>
2023-05-19 17:15:45 +0900

    Category Component Flatlist로 수정

--- src/util/interface.ts
+++ src/util/interface.ts
@@ -1,112 +1,95 @@
-import {FlexAlignType, ViewStyle, TextStyle, ImageStyle} from 'react-native';
+import { FlexAlignType, ViewStyle, TextStyle, ImageStyle } from 'react-native';
 
 export interface ITextBtn {
-  text: string;
-  weight?:
-    | '500'
-    | 'normal'
-    | 'bold'
-    | '100'
-    | '200'
-    | '300'
-    | '400'
-    | '600'
-    | '700'
-    | '800'
-    | '900'
-    | undefined;
-  size?: number;
-  color?: string;
-  bgColor?: string;
-  pt?: number;
-  pb?: number;
-  pl?: number;
-  pr?: number;
-  radius?: number;
-  onPress?: () => void;
+    text: string;
+    weight?: '500' | 'normal' | 'bold' | '100' | '200' | '300' | '400' | '600' | '700' | '800' | '900' | undefined;
+    size?: number;
+    color?: string;
+    bgColor?: string;
+    pt?: number;
+    pb?: number;
+    pl?: number;
+    pr?: number;
+    radius?: number;
+    onPress?: () => void;
 }
 
 export interface IIcon {
-  name: string;
-  type: string;
-  color?: string;
-  size?: number;
-  onPress?: () => void;
+    name: string;
+    type: string;
+    color?: string;
+    size?: number;
+    onPress?: () => void;
 }
 
 export interface IImage {
-  path: object;
-  w: number;
-  h: number;
-  flexBasis?: number | 'auto';
-  resizeMode?: string;
-  onPress?: () => void;
-  radius?: number;
+    path: object;
+    w: number;
+    h: number;
+    flexBasis?: number | 'auto';
+    resizeMode?: string;
+    onPress?: () => void;
+    radius?: number;
 }
-interface IscrollData {
-  url: string;
-  title: string;
-  category: string;
-  time: string;
+interface IScrollData {
+    url: string;
+    title: string;
+    category: string;
+    time: string;
 }
 export interface IScrollView {
-  scrollData: Array<IscrollData>;
-  gapHorizental?: number;
-  gapVertical?: number;
+    scrollData: Array<IScrollData>;
+    gapHorizontal?: number;
+    gapVertical?: number;
 }
 export interface ICategory {
-  gapHorizental?: number;
-  gapVertical?: number;
+    gapHorizontal?: number;
+    gapVertical?: number;
 }
 
 export interface IHeaderView {
-  title?: string;
+    title?: string;
 }
 
 export interface IFlexView {
-  children?: React.ReactNode;
-  flexDirection?:
-    | 'row'
-    | 'column'
-    | 'row-reverse'
-    | 'column-reverse'
-    | undefined;
-  justifyContent?:
-    | 'flex-start'
-    | 'flex-end'
-    | 'center'
-    | 'space-between'
-    | 'space-around'
-    | 'space-evenly'
-    | undefined;
-  alignItems?: FlexAlignType | undefined;
-  position?: 'absolute' | 'relative' | undefined;
-  t?: number;
-  r?: number;
-  w?: string | number;
-  h?: string | number;
-  ml?: number | undefined;
-  mt?: number | undefined;
-  mr?: number | undefined;
-  mb?: number | undefined;
-  gap?: number | undefined;
-  onPress?: () => void;
+    children?: React.ReactNode;
+    flexDirection?: 'row' | 'column' | 'row-reverse' | 'column-reverse' | undefined;
+    justifyContent?:
+        | 'flex-start'
+        | 'flex-end'
+        | 'center'
+        | 'space-between'
+        | 'space-around'
+        | 'space-evenly'
+        | undefined;
+    alignItems?: FlexAlignType | undefined;
+    position?: 'absolute' | 'relative' | undefined;
+    t?: number;
+    r?: number;
+    w?: string | number;
+    h?: string | number;
+    ml?: number | undefined;
+    mt?: number | undefined;
+    mr?: number | undefined;
+    mb?: number | undefined;
+    gap?: number | undefined;
+    onPress?: () => void;
 }
 
 interface ICardData {
-  url: string | '';
-  title: string;
-  source: string;
+    url: string | '';
+    title: string;
+    source: string;
 }
 export interface ICardView {
-  cardData: Array<ICardData>;
-  color?: string;
-  size?: number;
-  onPress?: () => void;
+    cardData: Array<ICardData>;
+    color?: string;
+    size?: number;
+    onPress?: () => void;
 }
 export interface IHeaderTitle {
-  title: string;
-  color?: string;
-  size?: number;
-  onPress?: () => void;
+    title: string;
+    color?: string;
+    size?: number;
+    onPress?: () => void;
 }
